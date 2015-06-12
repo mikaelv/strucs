a6741bfdbdfb305434d09efa87a984d5895ff543
Mikael Valot <mikael.valot@gmail.com>
2015-06-12 09:11:34 +0100

    automatically materialize Wrapper instances

--- strucs-core/src/main/scala/org/strucs/Wrapper.scala
+++ strucs-core/src/main/scala/org/strucs/Wrapper.scala
@@ -1,5 +1,8 @@
 package org.strucs
 
+import scala.reflect.macros.whitebox
+import scala.language.experimental.macros
+
 /** typeclass for wrapper types, such as case class Name(v: String)
   * a wrapper can be built from a value, and we can extract a value from it
   * @tparam W type of the Wrapper
@@ -11,11 +14,27 @@ trait Wrapper[W, V] {
 }
 
 object Wrapper {
+  def macroImpl[W: c.WeakTypeTag, V : c.WeakTypeTag](c: whitebox.Context) = {
+    import c.universe._
+    val wsym = c.weakTypeOf[W].typeSymbol
+    val vsym = c.weakTypeOf[V].typeSymbol
 
+    if (!wsym.isClass || !wsym.asClass.isCaseClass) c.abort(c.enclosingPosition, s"$wsym is not a case class")
+    val fields = wsym.typeSignature.decls.toList.collect{ case x: TermSymbol if x.isVal && x.isCaseAccessor => x }
+    if (fields.size != 1) c.abort(c.enclosingPosition, s"$wsym must have exactly one field")
+    val field = fields.head.getter
+
+    val expr = q"new org.strucs.CaseClassWrapper[$wsym, $vsym](new $wsym(_), _.$field)"
+    //c.info(c.enclosingPosition, expr.toString, true)
+    expr
+  }
+
+  /** Automatically creates a Wrapper for case classes */
+  implicit def materializeWrapper[W, V]: Wrapper[W, V] = macro macroImpl[W, V]
 }
 
-/** TODO use a macro materializer to define this automatically */
-class ValueClassWrapper[W, V](_apply: V => W, _value: W => V) extends Wrapper[W, V] {
+/** Implementation of a Wrapper for a case class */
+class CaseClassWrapper[W, V](_apply: V => W, _value: W => V) extends Wrapper[W, V] {
 
   override def value(w: W): V = _value(w)
 
--- strucs-fix/src/main/scala/com/strucs/fix/dict/fix42/package.scala
+++ strucs-fix/src/main/scala/com/strucs/fix/dict/fix42/package.scala
@@ -2,8 +2,7 @@ package com.strucs.fix.dict
 
 import com.strucs.fix.FixCodec
 import com.strucs.fix.FixCodec.FixTag
-import FixCodec._
-import org.strucs.{ValueClassWrapper, Wrapper}
+import org.strucs.Wrapper.materializeWrapper
 
 /**
  * Tag names for FIX 4.2
@@ -11,7 +10,6 @@ import org.strucs.{ValueClassWrapper, Wrapper}
 package object fix42 {
   case class BodyLength(v: Int) extends AnyVal
   object BodyLength {
-    implicit val wrapper: Wrapper[BodyLength, Int] = new ValueClassWrapper(BodyLength.apply, _.v)
     implicit val codec: FixCodec[BodyLength] = new FixTag[BodyLength, Int](145)
   }
 }
